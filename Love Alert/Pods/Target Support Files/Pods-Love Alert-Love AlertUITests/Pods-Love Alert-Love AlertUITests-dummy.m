cce0a4bf9012cc64395872c697a49951ed7acba3
Henry Chen <hchen113@binghamton.edu>
2019-12-06 18:20:26 -0500

    firebase configured

---
+++ Love Alert/Pods/Target Support Files/Pods-Love Alert-Love AlertUITests/Pods-Love Alert-Love AlertUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Love_Alert_Love_AlertUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Love_Alert_Love_AlertUITests
+@end
---
+++ Love Alert/Pods/Target Support Files/Pods-Love AlertTests/Pods-Love AlertTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Love_AlertTests : NSObject
+@end
+@implementation PodsDummy_Pods_Love_AlertTests
+@end
