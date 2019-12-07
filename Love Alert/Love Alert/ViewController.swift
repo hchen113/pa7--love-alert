9517c2355f575a0c5ab9c1af64f80f450bea94a8
Henry Chen <hchen113@binghamton.edu>
2019-12-06 19:10:48 -0500

    pods update

--- Love Alert/Love Alert/ViewController.swift
+++ Love Alert/Love Alert/ViewController.swift
@@ -1,14 +1,25 @@
 import UIKit
 import MapKit
+import Firebase
+import FirebaseDatabase
+import GeoFire
 import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var top_text: UILabel!
     @IBOutlet weak var map: MKMapView!
     fileprivate let locationManager:CLLocationManager = CLLocationManager()
     
+    var geoFireRef: DatabaseReference?
+    var geoFire: GeoFire?
+    
     @IBAction func heart_clicked(_ sender: Any) {
+        
+        let userKey = UIDevice.current.identifierForVendor?.uuidString
+        let currentLocation = locationManager.location
+        geoFire!.setLocation(CLLocation(latitude: (currentLocation?.coordinate.latitude)! , longitude: (currentLocation?.coordinate.longitude)!), forKey: userKey!)
+        
         top_text.text = "You've just liked someone around you!"
         top_text.isHidden = false;
         _ = Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { (timer) in
@@ -21,11 +32,19 @@ class ViewController: UIViewController {
         locationManager.requestWhenInUseAuthorization()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.pausesLocationUpdatesAutomatically = false
+        locationManager.delegate = self
         locationManager.startUpdatingLocation()
         
         map.showsUserLocation = true;
         map.userTrackingMode = .follow
         top_text.isHidden = true;
+        
+        let geoFireRef = Database.database().reference()
+        let geoFire = GeoFire(firebaseRef: geoFireRef)
+        
+        
+        
     }
 
 
